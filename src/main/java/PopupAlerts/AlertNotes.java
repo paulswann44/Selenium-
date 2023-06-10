f073d626205f3aba58f61211b2a3d5cad4eefa5d
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2023-06-09 22:29:16 -0400

    confirmations

--- src/main/java/PopupAlerts/AlertNotes.java
+++ src/main/java/PopupAlerts/AlertNotes.java
@@ -10,7 +10,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AlertNotes {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
@@ -19,6 +19,8 @@ public class AlertNotes {
         String url = "https://the-internet.herokuapp.com/javascript_alerts";
         String successMessage = "You successfully clicked an alert";
         String buttonXPath = "//button[text()='Click for JS Alert']";
+        String confirmXPath = "//button[onclick='jsconfirm()']";
+        String cxld = "You clicked: Cancel";
 
         driver.get(url);
         driver.findElement(By.xpath(buttonXPath)).click();
@@ -26,6 +28,7 @@ public class AlertNotes {
         Alert alert = driver.switchTo().alert();
         String alertText = alert.getText();
         System.out.println(alertText);
+        Thread.sleep(5000);
         alert.accept();
 
         if (driver.getPageSource().contains(successMessage)) {
@@ -33,6 +36,20 @@ public class AlertNotes {
             System.out.println("============================");
         }
 
+        //js comfirm
+        driver.findElement(By.xpath(confirmXPath)).click();
+
+        Alert alert2 =driver.switchTo().alert();
+        System.out.println(alert2.getClass());
+        Thread.sleep(5000);
+        alert2.dismiss();
+        if (driver.getPageSource().contains(cxld)) {
+            System.out.println(cxld);
+            System.out.println("============================");
+        }
+
+
+        driver.close();
         driver.quit();
     }
 }
